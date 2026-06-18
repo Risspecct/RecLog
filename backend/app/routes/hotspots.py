a997d74a85ac6316a7964ffabdb5a86c13e85e8b
Risspecct <rishikgarg.dev@gmail.com>
2026-06-18 23:39:02 +0530

    feat: fetch individual hotspot details

--- backend/app/routes/hotspots.py
+++ backend/app/routes/hotspots.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
 
-from app.services.hotspot_service import (get_all_hotspots)
+from app.services.hotspot_service import (get_all_hotspots, get_hotspot_by_id)
 from app.models.hotspot import HotspotResponse
 
 router = APIRouter(
@@ -12,3 +13,16 @@ router = APIRouter(
 @router.get("", response_model=list[HotspotResponse])
 def get_hotspots(limit: int = 50, offset: int = 0):
     return get_all_hotspots(limit=limit, offset=offset)
+
+
+@router.get(
+    "/{h3_cell}",
+    response_model=HotspotResponse
+)
+def get_hotspot_details(h3_cell: str):
+    hotspot = get_hotspot_by_id(h3_cell)
+
+    if hotspot is None:
+        raise HTTPException(status_code=404, detail="Hotspot not found")
+
+    return hotspot
--- backend/app/services/hotspot_service.py
+++ backend/app/services/hotspot_service.py
@@ -1,13 +1,22 @@
 from app.services.data_service import get_dataset
 
 
-def get_all_hotspots(
-    limit: int = 50,
-    offset: int = 0
-):
+def get_all_hotspots(limit: int = 50, offset: int = 0):
     df = get_dataset()
 
     return (
         df.iloc[offset: offset + limit]
         .to_dict(orient="records")
     )
+
+
+def get_hotspot_by_id(h3_cell: str):
+
+    df = get_dataset()
+
+    hotspot = df[df["h3_cell"] == h3_cell]
+
+    if hotspot.empty:
+        return None
+
+    return hotspot.iloc[0].to_dict()
