27d0bbfa75b2f39a4248abc4ff0d988a8537c3f1
Risspecct <rishikgarg.dev@gmail.com>
2026-06-21 13:40:34 +0530

    resolved conflicts

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 import logging
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
 from app.services.data_service import load_datasets
 from app.routes.hotspots import router as hotspot_router
 from app.routes.dashboard import router as dashboard_router
 from app.routes.analytics import router as analytics_router
 from app.routes.copilot import router as copilot_router
-from dotenv import load_dotenv
+from app.routes.simulation import router as simulation_router
+
 
 load_dotenv()
-from app.routes.simulation import router as simulation_router
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,7 +35,8 @@ app.add_middleware(
 app.include_router(hotspot_router)
 app.include_router(dashboard_router)
 app.include_router(analytics_router)
-app.include_router(copilot_router)app.include_router(simulation_router)
+app.include_router(copilot_router)
+app.include_router(simulation_router)
 
 
 @app.on_event("startup")
